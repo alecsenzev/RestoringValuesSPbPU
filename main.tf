947c70ad4dd7b8e4ab495cd99c7cb5e0a4a79743
Nikita Aleksentsev <160411454+alecsenzev@users.noreply.github.com>
2026-02-25 18:49:04 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     openstack = {
-      source  = "terraform-provider-openstack/openstack"
+      source  = "registry.terraform.io/terraform-provider-openstack/openstack"
       version = ">= 1.54.0"
     }
   }
